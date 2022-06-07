74db5529c63e33475351d1d3064ce1f1a97bf6bd
Paul Rivera <juniorrivera81@yahoo.com>
2022-06-06 20:31:46 -0600

    commit tree 2862d3751115f3b4714c4f7e22f29462252b49f8

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll(".like-glyph");
 
+function likeCallback(e) {
+  const heart = e.target
+  mimicServerCall("http://mimicServer.example.com")
+    .then(function(){
+      if ( heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.className = ""
+      }
+    })
+    .catch(function(error) {
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() =>  modal.className = "hidden", 3000);
+    });
+}
+for(const glyph of hearts)  {
+  glyph.addEventListener("click", likeCallback)
+}
 
 
 
